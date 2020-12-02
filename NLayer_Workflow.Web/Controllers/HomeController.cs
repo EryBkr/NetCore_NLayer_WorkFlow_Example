9219c57957492ec94a58a5831cb7005cf5f98c9b
Eray Bakır <info@eraybakir.com>
2020-12-02 14:39:22 +0300

    Theme Added

---
+++ NLayer_Workflow.Web/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NLayer_Workflow.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NLayer_Workflow.Web/Startup.cs
+++ NLayer_Workflow.Web/Startup.cs
@@ -16,6 +16,7 @@ namespace NLayer_Workflow.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews(); //Added MVC
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +28,11 @@ namespace NLayer_Workflow.Web
             }
 
             app.UseRouting();
+            app.UseStaticFiles();//wwwroot dýþarýya açýldý
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();//Default Route Added
             });
         }
     }
