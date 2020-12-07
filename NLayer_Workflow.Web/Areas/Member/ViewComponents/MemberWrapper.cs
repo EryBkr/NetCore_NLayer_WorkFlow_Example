a85effe0de0a09b6d55c0d4e8d190641a813d58d
Eray Bakır <info@eraybakir.com>
2020-12-07 23:15:03 +0300

    Member Layout Fixed

---
+++ NLayer_Workflow.Web/Areas/Member/ViewComponents/MemberWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NLayer_Workflow.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer_Workflow.Web.Areas.Member.ViewComponents
+{
+    public class MemberWrapper : ViewComponent
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public MemberWrapper(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var userName = User.Identity.Name;
+            var user = userManager.FindByNameAsync(userName).Result;
+            return View(user);
+        }
+    }
+}
